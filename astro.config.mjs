039f9c05f255d637107ddf3ba12dc207d70777b5
Bert Yin <yinzh@oregonstate.edu>
2025-05-08 09:39:42 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import prefetch from "@astrojs/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://adulbrich.github.io",
-  base: "/demo-github-actions",
+  site: "https://bert-yin.github.io"",
+  base: "/CS312-demo-github-actions",
 });
